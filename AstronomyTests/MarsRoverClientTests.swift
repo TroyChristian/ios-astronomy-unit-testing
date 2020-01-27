c7cb631c565ff50289ee384604e3c2a1317cbd0d
Troy Christian <apostasy8@gmail.com>
2020-01-27 17:28:42 -0500

    Create AstronomyTests

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,12 @@
+//
+//  MarsRoverClientTests.swift
+//  AstronomyTests
+//
+//  Created by Lambda_School_Loaner_219 on 1/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+@testable import Astronomy
+
+
