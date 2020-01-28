3b12ca98474f06acc37c1db10e2e97aae439d805
Troy Christian <apostasy8@gmail.com>
2020-01-27 21:26:13 -0500

    Tests complete

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -7,6 +7,105 @@
 //
 
 import Foundation
+import XCTest
 @testable import Astronomy
 
-
+class MarsRoverClientTests: XCTestCase {
+    
+    func testFetchRoverNetworkData() {
+        var rover: MarsRover?
+        
+        let client = MarsRoverClient()
+        let resultsExpectation = expectation(description: "Waiting on Mars rover")
+        
+        client.fetchMarsRover(named: "Curiosity") { (possibleRover, error) in
+            resultsExpectation.fulfill()
+            rover = possibleRover
+    }
+        
+        wait(for: [resultsExpectation], timeout: 10)
+        
+        XCTAssertNotNil(rover)
+    
+    
+}
+    
+    func testfetchRoverNetworkData() {
+        var rover: MarsRover?
+        var error: Error?
+        
+        let client = MarsRoverClient()
+        
+        let resultsExpectation = expectation(description: "Waiting on Mars Rover")
+        
+        client.fetchMarsRover(named: "Curiosity") { (possibleRover, error) in
+            resultsExpectation.fulfill()
+            rover = possibleRover
+        }
+        
+        wait(for: [resultsExpectation], timeout: 10)
+        
+        XCTAssertNotNil(rover)
+    
+    
+    func testFetchRoverNetworkError() {
+        var error: Error?
+        
+        let client = MarsRoverClient()
+        let resultsExpectation = expectation(description: "Waiting on mars Rover")
+        
+        client.fetchMarsRover(named: "ajdoajdaskjd") { (possibleRover, possibleError) in
+            if let possibleError = possibleError {
+                error = possibleError
+            }
+            resultsExpectation.fulfill()
+        }
+        wait(for: [resultsExpectation], timeout: 10)
+        
+        XCTAssertNotNil(error)
+    }
+        
+        
+}
+    
+    func testFetchMarsRoverMockData() {
+        var mock = MockLoader()
+        mock.data = validRoverJSON
+        var rover: MarsRover?
+        
+        let client = MarsRoverClient(networkLoader: mock)
+        let resultsExpectation = expectation(description: "Wait on a Mars rover")
+        
+        client.fetchMarsRover(named: "Curiosity") { (possibleRover, possibleError) in
+            resultsExpectation.fulfill()
+            rover = possibleRover
+        }
+        
+        wait(for: [resultsExpectation], timeout: 10)
+        
+        XCTAssertNotNil(rover)
+    }
+    
+    func testFetchPhotosMockData() {
+        var mock = MockLoader()
+        mock.data = validSol1JSON
+       let rover = MarsRover(name: "Pac-Man", launchDate: Date(), landingDate: Date(), status: .complete, maxSol: 1, maxDate: Date(), numberOfPhotos: 1, solDescriptions: [SolDescription(sol: 1, totalPhotos: 1, cameras: [""])])
+        
+        var photos: [MarsPhotoReference]?
+        
+        let client = MarsRoverClient(networkLoader: mock)
+        let photoExpectation = expectation(description: "Waiting on Mars Rover Photos")
+        
+        client.fetchPhotos(from: rover, onSol: 1) { (references, error) in
+            if let error = error {
+                mock.error = error
+            }
+            photos = references
+            photoExpectation.fulfill()
+    }
+        wait(for: [photoExpectation], timeout: 10)
+        XCTAssertNotNil(photos)
+        XCTAssertNil(mock.error)
+        XCTAssertTrue(photos!.count > 0)
+}
+}
